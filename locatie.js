34d80475527d215d31499aa5474ac50f1f6418ec
Georgi1402 <134556022+Georgi1402@users.noreply.github.com>
2023-10-19 15:39:08 +0300

    Update locatie.js

--- locatie.js
+++ locatie.js
@@ -1,11 +1,11 @@
 get_geo_position();
 function on_success(position){
-document.getElementById("latitude").innerHTML = "Latitude = " + position.coords.latitude;
+document.getElementById("latitude").innerHTML="Latitude = " + position.coords.latitude;
 document.getElementById("longitude").innerHTML = "Longitude= " + position.coords.longitude;
 document.getElementById("acc").innerHTML = "Accuracy = "+position.coords.accuracy;
 document.getElementById("altitude").innerHTML = "Altitude = "+position.coords.altitude;
 var latlon = position.coords.latitude + "," + position.coords.longitude;
-var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+\"&zoom=14&size=400x300&key=AIzaSyCTETwJLdrqSdWpG7c0Jf6jrqjUwJLQaAQ";
+var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyAoXtU3FSwMI4GYF4DosXoLxjfqAuMmH7Y";
 document.getElementById("map").innerHTML = "<img src='"+img_url+"'>";
 }
 function on_error(e)
